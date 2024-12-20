905ea23da5c4dd8a15be286b97b8201be759cca1
jhonnyCarpenito <jhonnycarpenito@gmail.com>
2024-12-19 23:22:00 -0400

    add custom title to app head in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Jhonny Carpenito',
+    },
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['nuxt-aos', '@nuxtjs/tailwindcss']
